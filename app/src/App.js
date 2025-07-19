44bfc518e0d9ee432d2a7e89c078d93885948cd0
snomura <>
2025-07-19 20:27:52 +0900

    ToDoリスト作成

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [toDos, setToDos]   = useState([]);
+  const [input,  setInput]  = useState('');
+
+  const handleAdd = ()=>{
+    if(input.trim() === '') return;
+    if(toDos.length > 0){
+      var lastElm = toDos[toDos.length - 1];
+      var lastId  = lastElm.id;
+    }
+    else{
+      var lastId = 0;
+    }
+
+    const toDoSt = {
+      'id'    :lastId + 1,
+      'title' :input,
+      'isDone':false
+    };
+    setToDos([...toDos, toDoSt]);
+    setInput('');
+  };
+
+  const handleDone = (index)=>{
+    setToDos(toDos.map((todo,i)=> i===index ? {...todo, 'isDone':true}:todo));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type='text'
+        value={input}
+        onChange={(e)=>setInput(e.target.value)}
+        placeholder='ToDoを入力'
+      >
+      </input>
+      <button onClick={()=>handleAdd()}> 追加 </button>
+
+      <ul>
+        {toDos.map((todo, index) => (
+          <div key={index}>
+            <span style={{textDecoration:todo.isDone ? 'line-through':'none'}}>
+                {todo.id}. {todo.title}
+            </span>
+            {!todo.isDone && (
+              <button onClick={() => handleDone(index)}> 実施</button>
+            )}
+          </div>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
